4f009791b78e6c8b70f28cea853e56400751ae5e
Stefano <stefanocarraro00@gmail.com>
2023-12-12 09:35:12 +0100

    update 11/12

--- Pyomo_notes.py
+++ Pyomo_notes.py
@@ -1,6 +1,6 @@
 # NOTES ABOUT THE PYOMO LIBRARY
 
-# Abstract models "AbstractModel". An abstract model is a template or a blueprint for a mathematical programming problem.
+# ABSTRACT MODELS: "AbstractModel". An abstract model is a template or a blueprint for a mathematical programming problem.
 # It defines the structure of the problem without specifying numerical values. It is useful when the problem structure is known, 
 # but the actual data values are not fixed at the time of model creation. Data is added later, after the abstract model is defined, 
 # and the model is then instantiated
@@ -24,17 +24,36 @@ model.c = pyo.Param(model.J)                            # c is a 1D array indexe
 # the next line declares a variable indexed by the set J
 model.x = pyo.Var(model.J, domain=pyo.NonNegativeReals)
 
-def obj_expression(m):
+def obj_expression(m):                                  # Defining the function for the OF: (sum of c_j*x_j) to be minimized 
     return pyo.summation(m.c, m.x)
 
-model.OBJ = pyo.Objective(rule=obj_expression)
+model.OBJ = pyo.Objective(rule=obj_expression)          # Defining the OF as the function obj_expression defined above
+# The default sense is minimization. For maximization, the sense=pyo.maximize argument must be used
 
-def ax_constraint_rule(m, i):
+def ax_constraint_rule(m, i):                           # Defining the constraints: sum of a_ij*x_j >= b_i for all i in I
     # return the expression for the constraint for i
     return sum(m.a[i,j] * m.x[j] for j in m.J) >= m.b[i]
 
 # the next line creates one constraint for each member of the set model.I
-model.AxbConstraint = pyo.Constraint(model.I, rule=ax_constraint_rule)
+model.AxbConstraint = pyo.Constraint(model.I, rule=ax_constraint_rule)  # Defining the constraints as the function ax_constraint_rule defined above
+
+# To define the parameters of the model, we need to create a data file (data.dat) with the following format:
+#param m := 1 ;
+#param n := 2 ;
+#param a :=
+#1 1 3
+#1 2 4
+#;
+#param c:=
+#1 2
+#2 3
+#;
+#param b := 1 1 ;
+
+# then the model is run by typing the following command in the terminal:
+# pyomo solve --solver=glpk AbsModel.py Data.dat
+
+# IN THE RESULTS: Number in [] represent the time the model required for each step
 
 # ----------------------------------------------------------------------------------------------------------------
 
@@ -46,7 +65,23 @@ model.x = pyo.Var([1,2], domain=pyo.NonNegativeReals)  # Variables x[1] and x[2]
 model.OBJ = pyo.Objective(expr = 2*model.x[1] + 3*model.x[2])  # Objective function 2*x[1] + 3*x[2] to be minimized
 model.Constraint1 = pyo.Constraint(expr = 3*model.x[1] + 4*model.x[2] >= 1)    # Constraint 3*x[1] + 4*x[2] >= 1 
 
+# ----------------------------------------------------------------------------------------------------------------
 
+# SETs and RANGESETs: Sets are used to define the index sets for variables, parameters, and constraints.
+# https://pyomo.readthedocs.io/en/stable/pyomo_modeling_components/Sets.html
 
-import sys
-print(sys.path)
+model.A = pyo.Set() 
+model.D = pyo.RangeSet(5, 10)   # Set B is the set of integers from 5 to 10
+model.A.pprint()
+model.D.pprint()
+
+model.I = model.A | model.D # union
+model.J = model.A & model.D # intersection
+model.K = model.A - model.D # difference
+model.L = model.A ^ model.D # exclusive-or
+model.M = model.A * model.D # cross-product
+
+# Pyomo provies some prefedined sets. Useful are:
+model.A = pyo.PositiveReals     # Valid also negative
+model.B = pyo.PositiveIntegers  # Valid also negative
+model.C = pyo.NonPositiveReals  # Valid also NonNegative and with Integers
